dcc69e17563fed3917df28f6f5425d7f1961f70f
ivane98 <i.vardoshvili22@gmail.com>
2024-02-07 11:13:45 +0400

    0702

--- ship.py
+++ ship.py
@@ -3,12 +3,24 @@ import pygame
 class Ship:
     def __init__(self, ai_game):
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
-        self.image = pygame.image.load('images\ship.bmp')
+        self.image = pygame.image.load(r'images\ship.bmp')
         self.rect = self.image.get_rect()
-
+        self.x = float(self.rect.x)
         self.rect.midbottom = self.screen_rect.midbottom
+        self.move_right = False
+        self.move_left = False
+
+    def update(self):
+        if self.move_right:
+            self.x += self.settings.ship_speed
+        elif self.move_left:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
